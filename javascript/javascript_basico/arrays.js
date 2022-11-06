f83554fb170588719b25678a68c614cda639d473
koritsi <mina.skuld@protonmail.com>
2022-11-05 23:08:21 -0300

    Ingreso directorio javascript

---
+++ javascript/javascript_basico/arrays.js
@@ -0,0 +1,7 @@
+
+const lista = ["Mina", 38, false, new Date(1984, 2, 27), {
+    "titulo": "Nacidos de la bruma (Mistborn)",
+    "autor": "Brandon Sanderson",
+    "fecha": new Date(2006, 0, 1),
+    "url": "https://es.wikipedia.org/wiki/Nacidos_de_la_bruma",
+}];
