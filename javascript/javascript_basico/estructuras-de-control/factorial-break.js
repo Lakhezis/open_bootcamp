ba2f72d1c1aea759799bdb349c2aca5025075beb
koritsi <mina.skuld@protonmail.com>
2022-11-06 21:28:09 -0300

    "mejoré la indentación"

--- javascript/javascript_basico/estructuras-de-control/factorial-break.js
+++ javascript/javascript_basico/estructuras-de-control/factorial-break.js
@@ -1,8 +1,8 @@
 let multi = 1;
 let i = 1;
 while(true) {
-        multi *= (i + 1);
-        i ++;
-        if (i === 10) break
-    }
-    console.log(multi)
+    multi *= (i + 1);
+    i ++;
+    if (i === 10) break
+}
+console.log(multi)
